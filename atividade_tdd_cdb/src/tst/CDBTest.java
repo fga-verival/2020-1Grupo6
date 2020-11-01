6c78a00020417aac01683c8cb2bff4d204366c5e
Lucas <lucasmacielaguiar@gmail.com>
2020-10-31 21:59:49 -0300

    Calculo do Rendimento Bruto - Falta de implementacao
    Co-authored-by: Marcelo <santos46ma@gmail.com>
    Co-authored-by: Miguel <miguelpimentel2013@gmail.com>

---
+++ atividade_tdd_cdb/src/tst/CDBTest.java
@@ -0,0 +1,18 @@
+package tst;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class CDBTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testCalcularRendimentoBruto() {
+		fail("Not yet implemented");
+	}
+}
