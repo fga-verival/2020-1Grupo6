03497aa5b3dda2657bb2964a75716a63484ebde0
Miguel Pimentel <miguel.silva@tembici.com>
2020-11-01 19:45:46 -0300

    Calculo do Imposto de Renda - Barra verde apos implementacao para unidade
    Co-authored-by: Marcelo <santos46ma@gmail.com>
    Co-authored-by: Marcos <marcoslimaraimundo@gmail.com>

--- atividade_tdd_cdb/src/app/CDBImpostos.java
+++ atividade_tdd_cdb/src/app/CDBImpostos.java
@@ -3,12 +3,29 @@ package app;
 public class CDBImpostos {
 	
 	private double rendimentoBruto;
+	private int qtdDias;
 
 	public void cadastrarRendimentoBruto(double rendimentoBruto) {
 		this.rendimentoBruto = rendimentoBruto;
  	}
 	
+	public void cadastrarQtdDias(int qtdDias) {
+		this.qtdDias = qtdDias;
+ 	}
+	
 	public double calcularImpostos() {
-		return 3.14; // Falsificação
+		double valor;
+		
+		if(this.qtdDias <= 180) {
+			valor = rendimentoBruto * (22.5/100.0);
+		} else if (this.qtdDias <= 360) {
+			valor = rendimentoBruto * (20/100.0);
+		} else if (this.qtdDias <= 720) {
+			valor = rendimentoBruto * (17.5/100.0);
+		} else {
+			valor = rendimentoBruto * (15.0/100.0);
+		}
+		
+		return Double.valueOf(String.format("%1.2f", valor)); 
 	}
 }
--- atividade_tdd_cdb/src/tst/CDBImpostosTest.java
+++ atividade_tdd_cdb/src/tst/CDBImpostosTest.java
@@ -24,6 +24,8 @@ import app.CDBImpostos;
 		cdb.cadastrarQtdDias(60);
 		
 		CDBImpostos cdbImpostos = new CDBImpostos();
+		cdbImpostos.cadastrarRendimentoBruto(cdb.getRendimentoBruto());
+		cdbImpostos.cadastrarQtdDias(60);
 		
 		assertEquals(3.14, cdbImpostos.calcularImpostos(), 0.05);
  	}
@@ -35,7 +37,48 @@ import app.CDBImpostos;
 		cdb.cadastrarQtdDias(120);
 		
 		CDBImpostos cdbImpostos = new CDBImpostos();
+		cdbImpostos.cadastrarRendimentoBruto(cdb.getRendimentoBruto());
+		cdbImpostos.cadastrarQtdDias(120);
 		
 		assertEquals(2.96, cdbImpostos.calcularImpostos(), 0.05);
+ 	}
+	
+	@Test
+ 	public void testCalcularImpostoDeRenda2() {
+ 		cdb.cadastrarValorInicial(100f);
+		cdb.cadastrarTaxaJuros(7.5);
+		cdb.cadastrarQtdDias(390);
+		
+		CDBImpostos cdbImpostos = new CDBImpostos();
+		cdbImpostos.cadastrarRendimentoBruto(cdb.getRendimentoBruto());
+		cdbImpostos.cadastrarQtdDias(390);
+		
+		assertEquals(1.40, cdbImpostos.calcularImpostos(), 0.05);
+ 	}
+	
+	@Test
+ 	public void testCalcularImpostoDeRenda3() {
+ 		cdb.cadastrarValorInicial(3000f);
+		cdb.cadastrarTaxaJuros(9.0);
+		cdb.cadastrarQtdDias(240);
+		
+		CDBImpostos cdbImpostos = new CDBImpostos();
+		cdbImpostos.cadastrarRendimentoBruto(cdb.getRendimentoBruto());
+		cdbImpostos.cadastrarQtdDias(240);
+		
+		assertEquals(35.51, cdbImpostos.calcularImpostos(), 0.05);
+ 	}
+	
+	@Test
+ 	public void testCalcularImpostoDeRenda4() {
+ 		cdb.cadastrarValorInicial(4200f);
+		cdb.cadastrarTaxaJuros(9.5);
+		cdb.cadastrarQtdDias(900);
+		
+		CDBImpostos cdbImpostos = new CDBImpostos();
+		cdbImpostos.cadastrarRendimentoBruto(cdb.getRendimentoBruto());
+		cdbImpostos.cadastrarQtdDias(900);
+		
+		assertEquals(147.58, cdbImpostos.calcularImpostos(), 0.05);
  	}
  }
