756bc3e013af531edf077f56cccde2327214a4d1
Miguel Pimentel <miguel.silva@tembici.com>
2020-11-01 18:44:39 -0300

    Calculo do Imposto de Renda - Falta de implementacao
    Co-authored-by: Marcelo <santos46ma@gmail.com

---
+++ atividade_tdd_cdb/src/tst/CDBImpostosTest.java
@@ -0,0 +1,18 @@
+package tst;
+
+ import static org.junit.Assert.*;
+
+ import org.junit.Before;
+ import org.junit.Test;
+
+ public class CDBImpostosTest {
+
+ 	@Before
+ 	public void setUp() throws Exception {
+ 	}
+
+ 	@Test
+ 	public void testCalcularImpostoDeRenda() {
+ 		fail("Not yet implemented");
+ 	}
+ }
