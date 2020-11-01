6c5df27c7559081736e926eab97fdadabcb00a1e
Lucas <lucasmacielaguiar@gmail.com>
2020-11-01 00:04:41 -0300

    Calculo do Rendimento Bruto - Triangulacao
    Co-authored-by: Marcelo <santos46ma@gmail.com>
    Co-authored-by: Miguel <miguelpimentel2013@gmail.com>

--- atividade_tdd_cdb/src/tst/CDBTest.java
+++ atividade_tdd_cdb/src/tst/CDBTest.java
@@ -9,15 +9,10 @@ import app.CDB;
 
 public class CDBTest {
 
-	private CDB cdb;
-
-	@Before
-	public void setUp() throws Exception {
-		cdb = new CDB();
-	}
-
 	@Test
 	public void testCalcularRendimentoBruto() {
+		CDB cdb = new CDB();
+		
 		cdb.cadastrarValorInicial(1000f);
 		cdb.cadastrarTaxaJuros(8.5);
 		cdb.cadastrarQtdDias(60);
@@ -26,10 +21,30 @@ public class CDBTest {
 	
 	@Test
 	public void testCalcularRendimentoBruto2() {
+		CDB cdb = new CDB();
+		
 		cdb.cadastrarValorInicial(500f);
 		cdb.cadastrarTaxaJuros(8.0);
 		cdb.cadastrarQtdDias(120);
 		
 		assertEquals(13.15, cdb.getRendimentoBruto(), 0.05);
+	}
+	
+	@Test
+	public void testCalcularRendimentoBruto3() {
+		CDB cdb1 = new CDB();
+		cdb1.cadastrarValorInicial(1000f);
+		cdb1.cadastrarTaxaJuros(8.5);
+		cdb1.cadastrarQtdDias(60);
+		
+		CDB cdb2 = new CDB();
+		cdb2.cadastrarValorInicial(500f);
+		cdb2.cadastrarTaxaJuros(8.0);
+		cdb2.cadastrarQtdDias(120);
+		
+		double rendimentoBruto1 = cdb1.getRendimentoBruto();
+		double rendimentoBruto2 = cdb2.getRendimentoBruto();
+		
+		assertEquals(27.12, rendimentoBruto1 + rendimentoBruto2, 0.05);
 	}
 }
