9255ee8f1374bf0550f0c5e1ed6e6346897169cc
Mexazonic <brunnohenriq697@gmail.com>
2020-11-30 11:59:06 -0300

    Add K-component tests
    Co-authored-by: Ernando Braga <nando_rhapsody@hotmail.com>

--- tf2_replaceability/app/fetch.js
+++ tf2_replaceability/app/fetch.js
@@ -1,16 +1,151 @@
 import fetch from 'node-fetch';
 
-const getUsers = async () => {
 
-    //const res = await fetch('https://api.github.com/users/miguelpimentel');
-    const res = await fetch('https://jsonplaceholder.typicode.com/users');
-    const user = await res.json();
 
-    return res.status;
+const getResponseData = async (url) => {
+    return new Promise((resolve, reject) => {
+       let status,config
+        fetch(url)
+        .then((res)=> {
+            status = res.status
+            config = res.headers
+            config.url = url
+            return res.json()    
+        }
+        )
+        
+        .then ((data => {
+        console.log(data)
+            if(status == 200)
+                resolve({status, data, config})
+            
+            else 
+                reject({status})
+        })
+        )
+        .catch(error => reject({response}));
+    });
 }
 
-getUsers();
-
-export default {
-    getUsers
+const postResponseData = async (url, body) => {
+    return new Promise((resolve, reject) => {
+        let status,config
+        fetch(url, {  
+        method: 'post',
+        body:  JSON.stringify(body),
+        headers: { 'Content-Type': 'application/json'}}
+        )
+        .then((res)=> {
+            status = res.status
+            config = res.headers
+            config.url = url
+            return res.json()    
+        }
+        )
+        
+        .then ((data => {
+        console.log(data)
+            if(status == 201)
+                resolve({status, data, config})
+            
+            else 
+                reject({status})
+        })
+        )
+        .catch(error => reject({response: error}));
+    });
 }
+
+const putResponseData = async (url, body) => {
+    return new Promise((resolve, reject) => {
+        let status,config
+        fetch(url, {  
+        method: 'put',
+        body:  JSON.stringify(body),
+        headers: { 'Content-Type': 'application/json'}}
+        )
+        .then((res)=> {
+            status = res.status
+            config = res.headers
+            config.url = url
+            return res.json()    
+        }
+        )
+        
+        .then ((data => {
+            if(status == 200){
+                console.log(status)
+                resolve({status, data, config})
+            }
+            else{ 
+                console.log(status)
+                reject({status})
+                
+            }
+            })
+        )
+        .catch(response => reject({response, status}));
+    });
+
+    }
+
+const patchResponseData = async (url, body) => {
+    return new Promise((resolve, reject) => {
+        let status,config
+        fetch(url, {  
+        method: 'put',
+        body:  JSON.stringify(body),
+        headers: { 'Content-Type': 'application/json'}}
+        )
+        .then((res)=> {
+            status = res.status
+            config = res.headers
+            config.url = url
+            return res.json()    
+        })
+        .then ((data => {
+            if(status == 200){
+                console.log(status)
+                resolve({status, data, config})
+            }
+            else{ 
+                console.log(status)
+                reject({status})                
+            }
+            }))
+        .catch(response => reject({response, status}));
+    });
+}
+
+const deleteResponseData = async (url) => {
+    return new Promise((resolve, reject) => {
+        let status,config
+         fetch(url)
+         .then((res)=> {
+             status = res.status
+             config = res.headers
+             config.url = url
+             return res.json()    
+         }
+         )
+         
+         .then ((data => {
+         console.log(data)
+             if(status == 200)
+                 resolve({status, data, config})
+             
+             else 
+                 reject({status})
+         })
+         )
+         .catch(error => reject({response}));
+     });
+    }
+
+export {
+    getResponseData,
+    putResponseData,
+    patchResponseData,
+    postResponseData,
+    deleteResponseData
+};
